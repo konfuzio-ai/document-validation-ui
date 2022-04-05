559331ba1c869bcf7731556282a1d2a949ed95cd
Pedro Dinis <pdinisricardo@gmail.com>
2022-04-05 20:45:29 +0100

    Add sentry configuration

--- src/main.js
+++ src/main.js
@@ -5,8 +5,30 @@ import Vue from "vue";
 import VueKonva from "vue-konva";
 import store from "./store";
 import App from "./App.vue";
+import * as Sentry from "@sentry/vue";
 
 Vue.use(VueKonva);
+
+// Sentry config
+if (process.env.NODE_ENV != "development") {
+  Sentry.init({
+    Vue,
+    dsn: process.env.VUE_APP_SENTRY_DSN,
+    integrations: [new Integrations.BrowserTracing()],
+    environment: process.env.NODE_ENV,
+
+    // We recommend adjusting this value in production, or using tracesSampler
+    // for finer control
+    tracesSampleRate: 1.0,
+
+    // If false, errors won't show up in devtools
+    logErrors: true,
+
+    tracingOptions: {
+      trackComponents: true
+    }
+  });
+}
 
 /**
  * Main entrypoint for the Document Dashboard.
