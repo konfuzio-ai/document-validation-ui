a28e8667984692e51073f5c12542e6b16b73f2d2
Pedro Dinis <pdinisricardo@gmail.com>
2022-05-09 23:10:39 +0100

    Integration of category api call and store

--- src/App.vue
+++ src/App.vue
@@ -9,7 +9,7 @@ body {
 </style>
 <template>
   <div>
-    <DocumentsList />
+    <DocumentsList v-if="showDocumentsList" />
     <DocumentDashboard class="dashboard-component" />
   </div>
 </template>
@@ -27,13 +27,31 @@ export default {
   },
   created() {
     // fetch info from API and save it on store
-    this.$store.dispatch("document/startLoading");
-    Promise.all([
-      this.$store.dispatch("document/fetchDocumentList"),
-      this.$store.dispatch("document/fetchAnnotations")
-    ]).finally(() => {
-      this.$store.dispatch("document/endLoading");
-    });
+    this.documentLoading();
+    if (process.env.VUE_APP_CATEGORY_ID) {
+      this.categoryLoading();
+    }
+  },
+  data() {
+    return {
+      showDocumentsList: process.env.VUE_APP_CATEGORY_ID
+    };
+  },
+  methods: {
+    documentLoading() {
+      this.$store.dispatch("document/startLoading");
+      Promise.all([this.$store.dispatch("document/fetchAnnotations")]).finally(
+        () => {
+          this.$store.dispatch("document/endLoading");
+        }
+      );
+    },
+    categoryLoading() {
+      Promise.all([
+        this.$store.dispatch("category/fetchCategory"),
+        this.$store.dispatch("category/fetchDocumentList")
+      ]);
+    }
   }
 };
 </script>
