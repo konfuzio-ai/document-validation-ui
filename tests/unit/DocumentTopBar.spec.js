63721baf626e5ca2cac3d7e7a96b943ccf2b597e
Veronica Barina <vebarina@gmail.com>
2022-05-27 11:42:54 +0300

    Adding more tests for top bar component

--- tests/unit/DocumentTopBar.spec.js
+++ tests/unit/DocumentTopBar.spec.js
@@ -1,4 +1,4 @@
-import { mount, shallowMount } from "@vue/test-utils";
+import { mount } from "@vue/test-utils";
 import { DocumentTopBar } from "../../src/components/DocumentTopBar";
 import store from "../../src/store";
 
@@ -6,7 +6,11 @@ import store from "../../src/store";
 const $t = () => {};
 
 describe("Document Top Bar", () => {
+  // Set file name
   const fileName = require("../mock/document_data.json").data_file_name;
+
+  // mock axios
+  jest.mock("axios");
 
   beforeEach(() => {
     Promise.resolve(store.dispatch("document/setFileName", fileName));
@@ -44,16 +48,6 @@ describe("Document Top Bar", () => {
     expect(button.text()).toBe("Save");
   });
 
-  it("Clicking on Edit button should make file extension not visible", async () => {
-    const wrapper = await mount(DocumentTopBar, {
-      store,
-      mocks: { $t },
-    });
-
-    await wrapper.findComponent(".edit-btn").trigger("click");
-    expect(wrapper.findComponent(".file-extension").exists()).toBe(false);
-  });
-
   it("No buttons should be visible while saving", async () => {
     const wrapper = mount(DocumentTopBar, {
       store,
@@ -63,7 +57,57 @@ describe("Document Top Bar", () => {
     await wrapper.findComponent(".edit-btn").trigger("click");
     expect(wrapper.findComponent(".edit-btn").exists()).toBe(false);
 
-    // await wrapper.findComponent(".save-btn").trigger("click");
-    // expect(wrapper.findComponent(".save-btn").exists()).toBe(false);
+    await wrapper.findComponent(".save-btn").trigger("click");
+    expect(wrapper.findComponent(".save-btn").exists()).toBe(false);
+  });
+
+  it("The file name should become a content editable when clicking on the Edit button and the file extension should not be visible", async () => {
+    const wrapper = mount(DocumentTopBar, {
+      store,
+      mocks: { $t },
+    });
+
+    await wrapper.findComponent(".edit-btn").trigger("click");
+    const contentEditable = wrapper.findComponent(".document-name");
+    expect(wrapper.findComponent(".file-extension").exists()).toBe(false);
+    expect(contentEditable.attributes("contenteditable")).toBe("true");
+  });
+
+  it("The file name should not be content editable after clicking the Save button", async () => {
+    const wrapper = mount(DocumentTopBar, {
+      store,
+      mocks: { $t },
+    });
+
+    await wrapper.findComponent(".edit-btn").trigger("click");
+    const contentEditable = wrapper.findComponent(".document-name");
+    await wrapper.findComponent(".save-btn").trigger("click");
+    expect(contentEditable.attributes("contenteditable")).toBe("false");
+  });
+
+  it("Clicking on the save button should show the autosaving message to the user", async () => {
+    const wrapper = mount(DocumentTopBar, {
+      store,
+      mocks: { $t },
+    });
+
+    await wrapper.findComponent(".edit-btn").trigger("click");
+    await wrapper.findComponent(".save-btn").trigger("click");
+    expect(wrapper.findComponent(".loading-container").exists()).toBe(true);
+  });
+
+  it("Check if save function is called after clicking save button", async () => {
+    const wrapper = mount(DocumentTopBar, {
+      store,
+      mocks: { $t },
+    });
+
+    const mockFn = jest.fn().mockName("saveFunction");
+
+    await wrapper.findComponent(".edit-btn").trigger("click");
+    await wrapper.findComponent(".save-btn").trigger("click");
+    await mockFn();
+
+    expect(mockFn).toHaveBeenCalledTimes(1);
   });
 });
