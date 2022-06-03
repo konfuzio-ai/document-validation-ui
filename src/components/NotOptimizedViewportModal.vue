1e61fd1e83c4796d584135b9fd031c14ec6910e5
Veronica Barina <vebarina@gmail.com>
2022-06-03 16:32:28 +0300

    Adding modal when viewport not optimized

---
+++ src/components/NotOptimizedViewportModal.vue
@@ -0,0 +1,46 @@
+<style
+  scoped
+  lang="scss"
+  src="../assets/scss/document_viewport_modal.scss"
+></style>
+
+<template>
+  <section>
+    <b-modal v-model="isModalActive" :can-cancel="['x']">
+      <div class="modal-card">
+        <div class="image">
+          <Illustration />
+        </div>
+        <div class="content">
+          <h3>{{ $t("small_viewport_title") }}</h3>
+          <p>{{ $t("small_viewport_info") }}</p>
+
+          <button class="ok-btn" @click="handleNotOptimizedModal">
+            {{ $t("ok") }}
+          </button>
+        </div>
+      </div>
+    </b-modal>
+  </section>
+</template>
+
+<script>
+import Illustration from "../assets/images/NotOptimizedIllustration.vue";
+
+export default {
+  name: "NotOptimizedViewportModal",
+  data() {
+    return {
+      isModalActive: true
+    };
+  },
+  components: {
+    Illustration
+  },
+  methods: {
+    handleNotOptimizedModal() {
+      this.isModalActive = false;
+    }
+  }
+};
+</script>
