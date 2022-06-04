2743b465b92a265fc0049a19a829d38ac341d6cc
Veronica Barina <vebarina@gmail.com>
2022-06-04 19:54:02 +0300

    Adding Buefy component for error message

--- src/components/DocumentTopBar/DocumentTopBar.vue
+++ src/components/DocumentTopBar/DocumentTopBar.vue
@@ -11,6 +11,20 @@
       <DocumentName :dataFileName="selectedDocument.data_file_name" />
     </div>
     <div class="handover"><DocumentHandover /></div>
+    <transition name="slide-fade">
+      <div v-if="showError" class="message">
+        <b-message
+          class="is-danger danger-msg message-body-border-color message-body-padding"
+        >
+          <div class="message-container">
+            {{ $t("error_message") }}
+            <div @click="handleErrorClose" class="btn-container">
+              <CloseBtnImg class="close-btn" />
+            </div>
+          </div>
+        </b-message>
+      </div>
+    </transition>
   </div>
 </template>
 
@@ -19,18 +33,30 @@ import DocumentDatasetStatus from "./DocumentDatasetStatus.vue";
 import DocumentCategory from "./DocumentCategory.vue";
 import DocumentName from "./DocumentName.vue";
 import DocumentHandover from "./DocumentHandover.vue";
+import CloseBtnImg from "../../assets/images/CloseBtnImg.vue";
 import { mapState } from "vuex";
 
 export default {
   name: "DocumentTopBar",
+  data() {
+    return {
+      showError: false
+    };
+  },
   components: {
     DocumentCategory,
     DocumentDatasetStatus,
     DocumentName,
-    DocumentHandover
+    DocumentHandover,
+    CloseBtnImg
   },
   computed: {
     ...mapState("document", ["selectedDocument"])
+  },
+  methods: {
+    handleErrorClose() {
+      this.showError = false;
+    }
   }
 };
 </script>
