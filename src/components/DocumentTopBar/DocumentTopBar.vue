8f4f506843409303d10de69b9ea40e1bb3dff3d4
Veronica Barina <vebarina@gmail.com>
2022-06-05 22:40:42 +0300

    Show message for category & status errors

--- src/components/DocumentTopBar/DocumentTopBar.vue
+++ src/components/DocumentTopBar/DocumentTopBar.vue
@@ -1,26 +1,33 @@
 <style scoped lang="scss" src="../../assets/scss/document_top_bar.scss"></style>
 
 <template>
-  <div class="document-top-bar" v-if="selectedDocument">
-    <div class="left-bar-components">
-      <DocumentCategory :selectedDocument="selectedDocument" />
-      <DocumentDatasetStatus :datasetStatus="selectedDocument.dataset_status" />
-    </div>
+  <div class="document-top-bar-component" v-if="selectedDocument">
+    <div class="document-top-bar">
+      <div class="left-bar-components">
+        <DocumentCategory
+          :selectedDocument="selectedDocument"
+          :handleShowError="handleShowError"
+          :handleMessage="handleMessage"
+        />
+        <DocumentDatasetStatus
+          :datasetStatus="selectedDocument.dataset_status"
+          :handleShowError="handleShowError"
+        />
+      </div>
 
-    <div class="document-name-container">
-      <DocumentName :dataFileName="selectedDocument.data_file_name" />
+      <div class="document-name-container">
+        <DocumentName :dataFileName="selectedDocument.data_file_name" />
+      </div>
+      <div class="handover"><DocumentHandover /></div>
     </div>
-    <div class="handover"><DocumentHandover /></div>
     <transition name="slide-fade">
-      <div v-if="showError" class="message">
-        <b-message
-          class="is-danger danger-msg message-body-border-color message-body-padding"
-        >
+      <div v-if="showError" class="error-message">
+        <b-message class="">
           <div class="message-container">
-            {{ $t("error_message") }}
-            <div @click="handleErrorClose" class="btn-container">
-              <CloseBtnImg class="close-btn" />
-            </div>
+            {{ categoryError ? $t("category_error") : $t("status_error") }}
+          </div>
+          <div @click="handleErrorClose" class="btn-container">
+            <CloseBtn class="close-btn" />
           </div>
         </b-message>
       </div>
@@ -33,14 +40,15 @@ import DocumentDatasetStatus from "./DocumentDatasetStatus.vue";
 import DocumentCategory from "./DocumentCategory.vue";
 import DocumentName from "./DocumentName.vue";
 import DocumentHandover from "./DocumentHandover.vue";
-import CloseBtnImg from "../../assets/images/CloseBtnImg.vue";
+import CloseBtn from "../../assets/images/CloseBtnWhite.vue";
 import { mapState } from "vuex";
 
 export default {
   name: "DocumentTopBar",
   data() {
     return {
-      showError: false
+      showError: false,
+      categoryError: false
     };
   },
   components: {
@@ -48,14 +56,21 @@ export default {
     DocumentDatasetStatus,
     DocumentName,
     DocumentHandover,
-    CloseBtnImg
+    CloseBtn
   },
   computed: {
     ...mapState("document", ["selectedDocument"])
   },
   methods: {
+    handleShowError() {
+      this.showError = true;
+    },
+    handleMessage() {
+      this.categoryError = true;
+    },
     handleErrorClose() {
       this.showError = false;
+      this.categoryError = false;
     }
   }
 };
