1ce5f91a0f5b47c323e554ea0412a9e01dca4d7c
Veronica Barina <vebarina@gmail.com>
2022-06-07 15:33:00 +0300

    Creating toolbar component

---
+++ src/components/DocumentPage/DocumentToolbar.vue
@@ -0,0 +1,39 @@
+<style scoped lang="scss" src="../../assets/scss/document_toolbar.scss"></style>
+
+<template>
+  <div class="toolbar-container">
+    <div class="toolbar">
+      <div class="icons icons-left">
+        <RotateIcon />
+        <FitZoomIcon />
+      </div>
+      <div class="icons icons-right">
+        <PlusIcon />
+        <MinusIcon />
+        <div class="percentage">{{ `${currentPercentage}%` }}</div>
+      </div>
+    </div>
+  </div>
+</template>
+
+<script>
+import RotateIcon from "../../assets/images/RotateIcon.vue";
+import FitZoomIcon from "../../assets/images/FitZoomIcon.vue";
+import PlusIcon from "../../assets/images/PlusIcon.vue";
+import MinusIcon from "../../assets/images/MinusIcon.vue";
+
+export default {
+  name: "Toolbar",
+  components: {
+    RotateIcon,
+    FitZoomIcon,
+    PlusIcon,
+    MinusIcon
+  },
+  data() {
+    return {
+      currentPercentage: 100
+    };
+  }
+};
+</script>
