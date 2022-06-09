e1f91cecc47b8334bb1bb8ad1919ad5cdcd05e10
Veronica Barina <vebarina@gmail.com>
2022-06-09 13:24:02 +0300

    Adding zoom functionalities + style

--- src/components/DocumentPage/DocumentToolbar.vue
+++ src/components/DocumentPage/DocumentToolbar.vue
@@ -4,12 +4,20 @@
   <div class="toolbar-container">
     <div class="toolbar">
       <div class="icons icons-left">
-        <RotateIcon />
-        <FitZoomIcon />
+        <div class="rotate icon">
+          <RotateIcon />
+        </div>
+        <div class="fit-zoom icon" @click.prevent.stop="fitAuto">
+          <FitZoomIcon />
+        </div>
       </div>
       <div class="icons icons-right">
-        <PlusIcon />
-        <MinusIcon />
+        <div class="zoom-in icon" @click.prevent.stop="zoomIn">
+          <PlusIcon />
+        </div>
+        <div class="zoom-out icon" @click.prevent.stop="zoomOut">
+          <MinusIcon />
+        </div>
         <div class="percentage">{{ `${currentPercentage}%` }}</div>
       </div>
     </div>
@@ -17,6 +25,7 @@
 </template>
 
 <script>
+import { mapState } from "vuex";
 import RotateIcon from "../../assets/images/RotateIcon.vue";
 import FitZoomIcon from "../../assets/images/FitZoomIcon.vue";
 import PlusIcon from "../../assets/images/PlusIcon.vue";
@@ -32,8 +41,34 @@ export default {
   },
   data() {
     return {
-      currentPercentage: 100
+      currentPercentage: 100,
+      increment: 0.25
     };
+  },
+  computed: {
+    ...mapState("display", ["scale"])
+  },
+  methods: {
+    zoomIn() {
+      this.updateScale(this.scale + this.increment);
+      this.currentPercentage += this.increment * 100;
+    },
+    zoomOut() {
+      if (this.scale <= this.increment) return;
+      this.updateScale(this.scale - this.increment);
+      this.currentPercentage -= this.increment * 100;
+    },
+    updateScale(scale) {
+      this.$store.dispatch("display/updateScale", { scale });
+      this.$store.dispatch("display/updateFit", "undefined");
+    },
+    fitAuto() {
+      if (this.currentPercentage === 100) {
+        return;
+      }
+      this.$store.dispatch("display/updateFit", "auto");
+      this.currentPercentage = 90;
+    }
   }
 };
 </script>
