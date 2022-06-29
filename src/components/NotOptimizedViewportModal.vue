c717fb47ac2828e2a24cdcdf95a20bd8c178bba6
Pedro Dinis <pdinisricardo@gmail.com>
2022-06-29 22:00:31 +0100

    Improvements to viewport modal

--- src/components/NotOptimizedViewportModal.vue
+++ src/components/NotOptimizedViewportModal.vue
@@ -6,20 +6,26 @@
 
 <template>
   <section>
-    <b-modal v-model="isModalActive" :can-cancel="['x']">
-      <div class="modal-card">
+    <b-modal
+      class="viewport-modal"
+      :width="500"
+      v-model="isModalActive"
+      :can-cancel="['x']"
+    >
+      <section class="modal-card-body">
         <div class="image">
           <Illustration />
         </div>
         <div class="content">
           <h3>{{ $t("small_viewport_title") }}</h3>
           <p>{{ $t("small_viewport_info") }}</p>
-
-          <button class="ok-btn" @click="handleNotOptimizedModal">
-            {{ $t("ok") }}
-          </button>
         </div>
-      </div>
+      </section>
+      <footer class="modal-card-foot">
+        <b-button type="is-primary" @click="handleNotOptimizedModal">
+          {{ $t("ok_got_it") }}
+        </b-button>
+      </footer>
     </b-modal>
   </section>
 </template>
