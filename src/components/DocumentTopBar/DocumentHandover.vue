243004eafbfec3641e6ebfca523cc77cfca9f96e
Veronica Barina <vebarina@gmail.com>
2022-06-29 14:36:43 +0300

    Added email error msg + dispatch to store

--- src/components/DocumentTopBar/DocumentHandover.vue
+++ src/components/DocumentTopBar/DocumentHandover.vue
@@ -28,13 +28,16 @@
             </div>
           </div>
           <div class="input-container">
-            <section class="input-section container">
+            <section
+              :class="['input-section container', invalidEmail && 'invalid']"
+            >
               <b-field type="input">
                 <b-input
                   :placeholder="$t('type_email')"
                   id="input"
                   autocomplete="off"
                   v-model="selected"
+                  @input="invalidEmail = false"
                 />
               </b-field>
               <b-loading
@@ -55,6 +58,9 @@
                 @click.prevent="handleHandover"
               />
             </section>
+            <div v-if="invalidEmail" class="invalid-email-msg">
+              <p>{{ $t("invalid_email") }}</p>
+            </div>
 
             <transition name="slide-fade">
               <div v-if="showError" class="error-message handover">
@@ -113,13 +119,26 @@ export default {
       showError: false,
       selected: null,
       scroll: false,
+      invalidEmail: false,
       regex:
         /^(([^<>()[\]\.,;:\s@\"]+(\.[^<>()[\]\.,;:\s@\"]+)*)|(\".+\"))@(([^<>()[\]\.,;:\s@\"]+\.)+[^<>()[\]\.,;:\s@\"]{2,})$/i
     };
   },
+  computed: {
+    // TODO: map members from store
+  },
   methods: {
     emailValidation(email) {
       return email.toLowerCase().match(this.regex);
+    },
+    isMember() {
+      const found = this.members.find(member => member.email === this.selected);
+
+      if (found) {
+        return true;
+      }
+
+      return false;
     },
     handleSelect(id, email) {
       const inputField = document.getElementById("input");
@@ -136,12 +155,59 @@ export default {
 
       // TODO: maybe have some msg if the email is not valid before handing over
       if (!this.emailValidation(this.selected)) {
-        console.log("email not valid");
+        this.invalidEmail = true;
         return;
       }
+
+      this.invalidEmail = false;
       this.isLoading = true;
 
       // TODO: dispatch to store to set the new owner of the doc
+      let assignee;
+
+      // Check if the user is already a member of the project or new member
+      if (this.isMember()) {
+        assignee = { assignee: `${this.selected}` };
+      } else {
+        assignee = { email: `${this.selected}`, role: "annotator" };
+      }
+
+      /** IF EXISTING MEMBER
+       * function already exists in store
+       */
+      // this.$store.dispatch("document/updateDocument", assignee)
+      // .then(response => {
+      //     // Check if the response is successfull or not
+      //     if (response) {
+      //       this.snackbar();
+      //       this.selected = null;
+      //     } else {
+      //       this.showError = true;
+      //     }
+      //   })
+      //   .finally(() => {
+      //     this.isLoading = false;
+      //   });
+
+      /** IF MEMBER
+       * Create new function in store
+       * POST request
+       * /api/v3/projects/ID/members/
+       */
+      // this.$store.dispatch("document/updateMembers", assignee)
+      // .then(response => {
+      //     // Check if the response is successfull or not
+      //     if (response) {
+      //       this.snackbar();
+      //       this.selected = null;
+      //     } else {
+      //       this.showError = true;
+      //     }
+      //   })
+      //   .finally(() => {
+      //     this.isLoading = false;
+      //   });
+
       setTimeout(() => {
         this.isLoading = false;
         this.snackbar();
@@ -150,15 +216,14 @@ export default {
       }, 2000);
     },
     snackbar() {
-      // TODO: figure out how to add the close icon next to the text
       this.$buefy.snackbar.open({
         message: `
-        ${this.$i18n.t("document_successfully_handed")} ${this.selected}
-        `,
+        ${this.$i18n.t("document_successfully_handed")} ${this.selected}`,
         actionText: null
       });
     }
   },
+
   updated() {
     if (this.members.length > 3) {
       this.scroll = true;
