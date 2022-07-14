212b18cd8d21b633ddf06be74af5a4623d018569
Pedro Dinis <pdinisricardo@gmail.com>
2022-07-14 13:58:17 +0100

    Implement edit mode view changes

---
+++ src/components/DocumentEdit/DocumentEdit.vue
@@ -0,0 +1,22 @@
+<style scoped lang="scss" src="../../assets/scss/document_edit.scss"></style>
+<template>
+  <div class="document-edit"><EditTopBar /></div>
+</template>
+
+<script>
+import { mapState } from "vuex";
+import EditTopBar from "./EditTopBar";
+/**
+ * This component shows a document thumbnail grid view to be able to edit the document.
+ */
+export default {
+  name: "DocumentEdit",
+  components: {
+    EditTopBar
+  },
+  computed: {
+    ...mapState("document", [])
+  },
+  methods: {}
+};
+</script>
