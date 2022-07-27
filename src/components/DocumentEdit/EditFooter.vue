f0071af2679f1530cbb3eb5051125c0b23e4c566
Veronica Barina <vebarina@gmail.com>
2022-07-27 18:02:30 +0300

    Created footer component to rotate all pages

---
+++ src/components/DocumentEdit/EditFooter.vue
@@ -0,0 +1,32 @@
+<style scoped lang="scss" src="../../assets/scss/document_edit.scss"></style>
+
+<template>
+  <div class="edit-footer">
+    <p>{{ $t("rotate_all") }}</p>
+    <div class="buttons-container">
+      <div class="action-icon" @click="rotateLeft">
+        <b-icon icon="arrow-rotate-left" class="is-small" />
+      </div>
+      <div class="action-icon" @click="rotateRight">
+        <b-icon icon="arrow-rotate-right" class="is-small" />
+      </div>
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+  name: "EditFooter",
+  data() {
+    return {};
+  },
+  methods: {
+    rotateLeft() {
+      this.$emit("rotate-left");
+    },
+    rotateRight() {
+      this.$emit("rotate-right");
+    }
+  }
+};
+</script>
