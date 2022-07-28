153547481ddc9abc930f671d4d0fbf8dca15bd03
Veronica Barina <vebarina@gmail.com>
2022-07-28 15:42:03 +0300

    Added and fixed tests

--- tests/unit/DocumentEdit.spec.js
+++ tests/unit/DocumentEdit.spec.js
@@ -1,10 +1,8 @@
-import {
-  shallowMount,
-  mount
-} from "@vue/test-utils";
+import { shallowMount, mount } from "@vue/test-utils";
 import {
   DocumentEdit,
-  EditTopBar
+  EditTopBar,
+  EditFooter,
 } from "../../src/components/DocumentEdit";
 import store from "../../src/store";
 
@@ -18,28 +16,41 @@ describe("Document Edit Component", () => {
         require("../mock/page_1.json"),
         require("../mock/page_2.json"),
       ]),
-      store
-      .dispatch("document/setEditMode", 'reorder'),
+      store.dispatch("document/setEditMode", "rotate"),
     ]);
   });
+
   it("check number of thumbnails", () => {
     const wrapper = shallowMount(DocumentEdit, {
       store,
       mocks: {
-        $t
+        $t,
       },
     });
     expect(wrapper.findAll(".image-section").length).toEqual(2);
   });
 
   it("check if cancelling close edit view", async () => {
-    const wrapper = mount(EditTopBar, {
+    const wrapper = mount(DocumentEdit, {
       store,
       mocks: {
-        $t
+        $t,
       },
     });
-    await wrapper.find(".edit-top-bar .buttons .button-cancel").trigger("click");
+    await wrapper
+      .find(".edit-top-bar .buttons .button-cancel")
+      .trigger("click");
+
     expect(store.state.document.editMode).toBe(null);
+  });
+
+  it("check if footer is shown in rotate mode", async () => {
+    const wrapper = mount(EditFooter, {
+      store,
+      mocks: {
+        $t,
+      },
+    });
+    expect(await wrapper.find(".edit-footer").exists()).toBe(true);
   });
 });
