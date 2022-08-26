392974ee6d4ab1e037c97d7de28c14fc1a8c0be8
Veronica Barina <vebarina@gmail.com>
2022-08-26 16:39:37 +0300

    Refactored top bar

--- src/components/DocumentTopBar/DocumentTopBar.vue
+++ src/components/DocumentTopBar/DocumentTopBar.vue
@@ -26,27 +26,10 @@
       </div>
 
       <div class="edit-mode-buttons" v-if="editMode">
-        <div class="buttons">
-          <b-button
-            :label="$t('cancel')"
-            class="button-cancel"
-            type="is-default"
-            @click="closeEditMode"
-          />
-          <b-button
-            :label="
-              editMode &&
-              updatedDocument &&
-              updatedDocument.length > 1 &&
-              !splitOverview
-                ? $t('next')
-                : $t('submit')
-            "
-            type="is-primary"
-            :disabled="false"
-            @click="handleButton"
-          />
-        </div>
+        <DocumentTopBarButtons
+          :handleShowError="handleShowError"
+          :handleMessage="handleMessage"
+        />
       </div>
     </div>
   </div>
@@ -57,10 +40,11 @@
  * This component has different functionalities
  * when on Dashboard View or Edit Mode
  */
-import DocumentDatasetStatus from "./DocumentDatasetStatus.vue";
-import DocumentCategory from "./DocumentCategory.vue";
-import DocumentName from "./DocumentName.vue";
-import DocumentHandover from "./DocumentHandover.vue";
+import DocumentDatasetStatus from "./DocumentDatasetStatus";
+import DocumentCategory from "./DocumentCategory";
+import DocumentName from "./DocumentName";
+import DocumentHandover from "./DocumentHandover";
+import DocumentTopBarButtons from "./DocumentTopBarButtons";
 import { mapState } from "vuex";
 
 export default {
@@ -80,7 +64,8 @@ export default {
     DocumentCategory,
     DocumentDatasetStatus,
     DocumentName,
-    DocumentHandover
+    DocumentHandover,
+    DocumentTopBarButtons
   },
   props: {
     showError: {
@@ -89,7 +74,7 @@ export default {
   },
   computed: {
     ...mapState("document", ["selectedDocument"]),
-    ...mapState("edit", ["editMode", "splitOverview", "updatedDocument"])
+    ...mapState("edit", ["editMode"])
   },
   methods: {
     handleShowError() {
@@ -97,91 +82,6 @@ export default {
     },
     handleShowMessage(message) {
       this.$emit("handle-message", message);
-    },
-
-    /** EDIT MODE */
-    closeEditMode() {
-      this.$store.dispatch("edit/disableEditMode").then(() => {
-        this.$store.dispatch("display/updateFit", "width");
-      });
-      this.$store.dispatch("edit/setSplitOverview", false);
-      this.$store.dispatch("edit/setUpdatedDocument", null);
-      this.$store.dispatch("edit/setSelectedPages", null);
-    },
-    handleButton() {
-      // Check if we are not in the split overview
-      // and if we have a split document
-      if (!this.splitOverview && this.updatedDocument.length > 1) {
-        // Enable the "next" button to go to the overview
-        this.$store.dispatch("edit/setSplitOverview", true);
-        this.$store.dispatch("edit/setSelectedPages", null);
-        return;
-      }
-
-      // If we are in the overview (so more than 1 doc)
-      // or in the edit mode (only 1 doc)
-      if (this.updatedDocument) {
-        this.$store.dispatch("document/startLoading");
-        this.$store.dispatch("document/startRecalculatingAnnotations");
-
-        // Send update request to the backend
-        this.$store
-          .dispatch("edit/editDocument", this.updatedDocument)
-          .then(response => {
-            const sleep = duration =>
-              new Promise(resolve => setTimeout(resolve, duration));
-            // Poll document data until the status_data is 111 (error) or
-            // 2 and labeling is available (done)
-            const pollUntilLabelingAvailable = duration => {
-              return this.$store
-                .dispatch("document/updateDocument", {})
-                .then(async () => {
-                  if (
-                    this.selectedDocument.status_data === 2 &&
-                    this.selectedDocument.labeling_available === 1
-                  ) {
-                    // set to null so DocumentLabelSets can reset it when watching
-                    // the new groupedAnnotationSets
-                    setTimeout(async () => {
-                      await this.$store.dispatch(
-                        "document/setActiveAnnotationSet",
-                        null
-                      );
-                      await this.$store.dispatch("document/fetchAnnotations");
-                      return true;
-                    }, 5000);
-                  } else if (this.selectedDocument.status_data === 111) {
-                    return false;
-                  } else {
-                    return sleep(duration).then(() =>
-                      pollUntilLabelingAvailable(duration)
-                    );
-                  }
-                });
-            };
-
-            // Check if the response is successfull or not
-            if (response) {
-              pollUntilLabelingAvailable(5000);
-            } else {
-              this.handleShowError();
-              this.handleMessage(this.$i18n.t("edit_error"));
-            }
-          })
-          .catch(error => {
-            console.log(error);
-            this.handleShowError();
-            this.handleMessage(this.$i18n.t("edit_error"));
-          })
-          .finally(async () => {
-            // Stop loading
-            await this.$store.dispatch("document/endLoading");
-            await this.$store.dispatch("document/endRecalculatingAnnotations");
-          });
-      }
-
-      // Close edit mode
-      this.closeEditMode();
     }
   }
 };
