94753630ad230c46cd0084ab33b97bd80d793bd5
Pedro Dinis <pdinisricardo@gmail.com>
2022-09-07 14:45:32 +0100

    Remove full mode validations

--- src/store/document.js
+++ src/store/document.js
@@ -22,7 +22,7 @@ const state = {
   // TODO: remove this after the reject label endpoint is merged in testing
   showRejectedLabels: process.env.VUE_APP_SHOW_REJECT_LABELS &&
     process.env.VUE_APP_SHOW_REJECT_LABELS == "true",
-  publicView: process.env.VUE_APP_GUEST_USER_TOKEN == null && process.env.VUE_APP_FULL_MODE != 'true',
+  publicView: process.env.VUE_APP_GUEST_USER_TOKEN == null,
   currentUser: null
 };
 
