856847f75f15ae3b9d24be03cc9ba545b077f2ca
Pedro Dinis <pdinisricardo@gmail.com>
2022-09-14 11:28:51 +0100

    Fix document scroll

--- src/components/DocumentPage/ScrollingDocument.vue
+++ src/components/DocumentPage/ScrollingDocument.vue
@@ -66,7 +66,7 @@ export default {
 
   methods: {
     updateScrollBounds() {
-      const { scrollTop, clientHeight } = this.$el;
+      const { scrollTop, clientHeight } = this.$refs.scrollingDocument;
       this.scrollTop = scrollTop;
       this.clientHeight = clientHeight;
     },
