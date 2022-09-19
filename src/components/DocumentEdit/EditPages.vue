04babcee4ed6e4dfcf47b05e1452c0e5ad24cb85
Veronica Barina <vebarina@gmail.com>
2022-09-19 14:02:02 +0300

    Fixed bugs

--- src/components/DocumentEdit/EditPages.vue
+++ src/components/DocumentEdit/EditPages.vue
@@ -156,6 +156,7 @@ export default {
         event.target.className === "sidebar" ||
         event.target.className === "sidebar-header" ||
         event.target.className === "edit-sidebar" ||
+        event.targe.className === "pages-section" ||
         event.target.nodeName === "CANVAS";
 
       if (clickedOutside) {
