250760dfafc6c124e04f2ff9fab74e2c1a7c3038
Veronica Barina <vebarina@gmail.com>
2023-01-05 12:45:06 +0200

    Created component for keyboard actions description

---
+++ src/components/DocumentTopBar/KeyboardActionsDescription.vue
@@ -0,0 +1,25 @@
+<template>
+  <div class="keyboard-actions-description">
+    <section class="b-tooltips">
+      <b-tooltip
+        label="Tooltip large multilined, because it's too long to be on a medium size. Did I tell you it's really long? Yes, it is — I assure you!"
+        position="is-left"
+        size="is-large"
+        multilined
+      >
+        <KeyboardIcon />
+      </b-tooltip>
+    </section>
+  </div>
+</template>
+
+<script>
+import KeyboardIcon from "../../assets/images/KeyboardIcon";
+
+export default {
+  name: "KeyboardActionsDescription",
+  components: {
+    KeyboardIcon,
+  },
+};
+</script>
