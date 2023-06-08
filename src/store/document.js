0055428233129e15899962973d2ca7596a85296e
Veronica Barina <vebarina@gmail.com>
2023-06-08 16:55:08 +0300

    Improvement when adding missing annotations state

--- src/store/document.js
+++ src/store/document.js
@@ -1240,10 +1240,27 @@ const mutations = {
   SET_MISSING_ANNOTATIONS: (state, missingAnnotations) => {
     state.missingAnnotations = missingAnnotations;
   },
-  ADD_MISSING_ANNOTATIONS: (state, missingAnnotations) => {
-    missingAnnotations.map((annotation) => {
-      state.missingAnnotations.push(annotation);
+  ADD_MISSING_ANNOTATIONS: (state, annotations) => {
+    annotations.map((annotation) => {
+      // check if already in missingAnnotations
+      const found = state.missingAnnotations.find(
+        (missingAnnotation) => missingAnnotation.id === annotation.id
+      );
+
+      if (found) {
+        const indexOfAnnotation = state.missingAnnotations.findIndex(
+          (missingAnnotation) => missingAnnotation.id === annotation.id
+        );
+
+        if (indexOfAnnotation > -1) {
+          state.missingAnnotations.splice(indexOfAnnotation, 1, annotation);
+        }
+      } else {
+        state.missingAnnotations.push(annotation);
+      }
     });
+
+    console.log("missing annotations", state.missingAnnotations);
   },
   DELETE_MISSING_ANNOTATION: (state, id) => {
     const indexOfAnnotationToDelete = state.missingAnnotations.findIndex(
