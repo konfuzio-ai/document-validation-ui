880653930d86e615c786b70c5c08b5b3f8c508d5
Pedro Dinis <pdinisricardo@gmail.com>
2023-06-09 14:28:56 +0100

    Fix Observer visibility error when running unit tests

--- tests/setup.js
+++ tests/setup.js
@@ -1,4 +1,27 @@
 import Vue from "vue";
 import Buefy from "buefy";
+import VueObserveVisibility from "vue-observe-visibility";
 
+Vue.use(VueObserveVisibility);
 Vue.use(Buefy);
+
+// Mock IntersectionObserver
+global.IntersectionObserver = class IntersectionObserver {
+  constructor() {}
+
+  disconnect() {
+    return null;
+  }
+
+  observe() {
+    return null;
+  }
+
+  takeRecords() {
+    return null;
+  }
+
+  unobserve() {
+    return null;
+  }
+};
