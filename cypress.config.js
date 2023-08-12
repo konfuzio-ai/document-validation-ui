25f69e2a784ffcf2cf1e3ada740faa7ce677b2c6
Pedro Dinis <pdinisricardo@gmail.com>
2023-08-12 01:01:04 +0100

    Cypress initial setup

---
+++ cypress.config.js
@@ -0,0 +1,10 @@
+module.exports = {
+  component: {
+    devServer: {
+      framework: "vue-cli",
+      bundler: "webpack",
+    },
+  },
+  viewportWidth: 1280,
+  viewportHeight: 720,
+};
--- src/main.js
+++ src/main.js
@@ -1,53 +1,13 @@
+import App from "./components/App";
 import Vue from "vue";
 import Buefy from "buefy";
 import VueKonva from "vue-konva";
-import App from "./components/App";
 import i18n from "./i18n";
 import store from "./store";
-import { library } from "@fortawesome/fontawesome-svg-core";
 import VueObserveVisibility from "vue-observe-visibility";
+import Icons from "./icons";
 
-// internal icons
-import {
-  faArrowUp,
-  faAngleRight,
-  faAngleLeft,
-  faAngleUp,
-  faAngleDown,
-  faSpinner,
-  faPlus,
-  faXmark,
-  faEllipsisVertical,
-  faCircleInfo,
-  faArrowRotateLeft,
-  faArrowRotateRight,
-  faScissors,
-  faRepeat,
-  faArrowLeft,
-  faArrowRight,
-} from "@fortawesome/free-solid-svg-icons";
-import { FontAwesomeIcon } from "@fortawesome/vue-fontawesome";
-
-library.add(
-  faArrowUp,
-  faAngleRight,
-  faAngleLeft,
-  faAngleUp,
-  faAngleDown,
-  faSpinner,
-  faXmark,
-  faSpinner,
-  faPlus,
-  faEllipsisVertical,
-  faCircleInfo,
-  faArrowRotateLeft,
-  faArrowRotateRight,
-  faScissors,
-  faRepeat,
-  faArrowLeft,
-  faArrowRight
-);
-Vue.component("VueFontawesome", FontAwesomeIcon);
+Vue.component("VueFontawesome", Icons);
 Vue.component("App", App);
 Vue.use(VueKonva);
 Vue.use(Buefy, {
