fd08e44f02bf8c84026190968fc12d5dafa5bb80
Veronica Barina <vebarina@gmail.com>
2023-08-29 10:16:21 +0300

    Added unit tests for download feature

--- tests/unit/DocumentPage.spec.js
+++ tests/unit/DocumentPage.spec.js
@@ -28,8 +28,61 @@ describe("Document Page Component", () => {
 
     expect(await wrapper.findComponent(".edit-icon").exists()).toBe(true);
     expect(await wrapper.findComponent(".edit-text").exists()).toBe(true);
+    expect(await wrapper.findComponent(".download-file").exists()).toBe(true);
     expect(await wrapper.findComponent(".fit-zoom").exists()).toBe(true);
     expect(await wrapper.findComponent(".zoom-in").exists()).toBe(true);
     expect(await wrapper.findComponent(".zoom-out").exists()).toBe(true);
+  });
+
+  it("Opens a menu when clicking the download icon in the toolbar", async () => {
+    const wrapper = render(ToolBar, false);
+
+    await wrapper
+      .findComponent(".download-file .dropdown .dropdown-trigger")
+      .trigger("click");
+
+    expect(await wrapper.findComponent(".dropdown-content").exists()).toBe(
+      true
+    );
+  });
+
+  it("Shows 2 options in the download menu", async () => {
+    const wrapper = render(ToolBar, false);
+
+    await wrapper
+      .findComponent(".download-file .dropdown .dropdown-trigger")
+      .trigger("click");
+
+    expect(await wrapper.findAllComponents(".dropdown-item").length).toBe(2);
+  });
+
+  it("Downloads file when clicking download menu option", async () => {
+    const wrapper = render(ToolBar, false);
+    const url = "https://testing.konfuzio.com/doc/show/1369630/";
+    const fileName = getData("document").selectedDocument.data_file_name;
+
+    const link = {
+      click: jest.fn(),
+    };
+
+    global.URL.createObjectURL = jest.fn(() => url);
+    global.URL.revokeObjectURL = jest.fn();
+    global.Blob = function (content, options) {
+      return { content, options };
+    };
+
+    jest.spyOn(document, "createElement").mockImplementation(() => link);
+
+    await wrapper
+      .findComponent(".download-file .dropdown .dropdown-trigger")
+      .trigger("click");
+    await wrapper.findAllComponents(".dropdown-item").at(1).trigger("click");
+
+    wrapper.vm.$nextTick(() => {
+      expect(wrapper.findComponent("a").exists()).toBe(true);
+      expect(link.href).toBe(url);
+      expect(link.download).toBe(fileName);
+      expect(link.click).toHaveBeenCalledTimes(1);
+    });
   });
 });
