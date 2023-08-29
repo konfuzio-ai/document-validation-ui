7c685e2e0d875ee5b32bcb6be3555652201744bf
Pedro Dinis <pdinisricardo@gmail.com>
2023-08-29 18:51:06 +0100

    Create spec

---
+++ src/components/DocumentThumbnails/DocumentThumbnails.cy.js
@@ -0,0 +1,7 @@
+import DocumentThumbnails from "./DocumentThumbnails.vue";
+
+describe("<DocumentThumbnails />", () => {
+  it("renders the app", () => {
+    cy.mount(DocumentThumbnails);
+  });
+});
