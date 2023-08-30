3df2f6299dd46db35cb5aa2d3f2e8e1e4d36c9d3
Veronica Barina <vebarina@gmail.com>
2023-08-30 14:08:01 +0300

    Remove unit test for download:
    - which will be replaced with API test in new library

--- tests/unit/DocumentPage.spec.js
+++ tests/unit/DocumentPage.spec.js
@@ -55,34 +55,4 @@ describe("Document Page Component", () => {
 
     expect(await wrapper.findAllComponents(".dropdown-item").length).toBe(2);
   });
-
-  it("Downloads file when clicking download menu option", async () => {
-    const wrapper = render(ToolBar, false);
-    const url = "https://testing.konfuzio.com/doc/show-original/1369630/";
-    const fileName = getData("document").selectedDocument.data_file_name;
-
-    const link = {
-      click: jest.fn(),
-    };
-
-    global.URL.createObjectURL = jest.fn(() => url);
-    global.URL.revokeObjectURL = jest.fn();
-    global.Blob = function (content, options) {
-      return { content, options };
-    };
-
-    jest.spyOn(document, "createElement").mockImplementation(() => link);
-
-    await wrapper
-      .findComponent(".download-file .dropdown .dropdown-trigger")
-      .trigger("click");
-    await wrapper.findAllComponents(".dropdown-item").at(0).trigger("click");
-
-    wrapper.vm.$nextTick(() => {
-      expect(wrapper.findComponent("a").exists()).toBe(true);
-      expect(link.href).toBe(url);
-      expect(link.download).toBe(fileName);
-      expect(link.click).toHaveBeenCalledTimes(1);
-    });
-  });
 });
