e1e96be93bbcfb66716b0de75fc765f713fcee0b
Veronica Barina <vebarina@gmail.com>
2023-09-15 11:09:22 +0300

    Added new commands for store actions

--- cypress/support/component.js
+++ cypress/support/component.js
@@ -46,6 +46,16 @@ Cypress.Commands.add("fetchDocument", () => {
   cy.wait(1000);
 });
 
+Cypress.Commands.add("fetchCategories", (projectId) => {
+  cy.store().invoke("dispatch", "category/fetchCategories", projectId);
+  cy.wait(1000);
+});
+
+Cypress.Commands.add("fetchDocumentList", (parameters) => {
+  cy.store().invoke("dispatch", "project/fetchDocumentList", parameters);
+  cy.wait(1000);
+});
+
 Cypress.Commands.add("storeState", (store, state) => {
   cy.store().its("state").its(store).its(state);
 });
