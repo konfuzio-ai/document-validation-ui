a0b6dabe7b9a6345d87e844b5687e23d7f1b1af4
Pedro Dinis <pdinisricardo@gmail.com>
2023-10-03 18:27:59 +0100

    Setup the document pages tests

---
+++ src/components/DocumentPage/DocumentPage.cy.js
@@ -0,0 +1,32 @@
+import DocumentDashboard from "../DocumentDashboard.vue";
+
+describe("Document Page", () => {
+  beforeEach(() => {
+    cy.fetchDocument();
+    cy.setFullMode();
+  });
+
+  it("Can click on an existing annotation", () => {
+    cy.mount(DocumentDashboard).then(({ wrapper, component }) => {
+      component.onDocumentResize();
+    });
+  });
+
+  it("Can click on an entity", () => {
+    cy.mount(DocumentDashboard).then(({ wrapper, component }) => {
+      component.onDocumentResize();
+    });
+  });
+
+  it("Can create a bounding box", () => {
+    cy.mount(DocumentDashboard).then(({ wrapper, component }) => {
+      component.onDocumentResize();
+    });
+  });
+
+  it("Hovering an annotation selects the annotation the annotation list", () => {
+    cy.mount(DocumentDashboard).then(({ wrapper, component }) => {
+      component.onDocumentResize();
+    });
+  });
+});
