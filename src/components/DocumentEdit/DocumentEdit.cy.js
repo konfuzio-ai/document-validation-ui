cca541f5619f8aace39420d2110e6bc3322fd0e6
Pedro Dinis <pdinisricardo@gmail.com>
2023-10-03 18:02:57 +0100

    Add drag and drop cypress test to document edit feature

--- src/components/DocumentEdit/DocumentEdit.cy.js
+++ src/components/DocumentEdit/DocumentEdit.cy.js
@@ -10,30 +10,26 @@ describe("Document Edit", () => {
   });
 
   it("Shows edit mode - split, rotate & reorder view", () => {
-    cy.get("#document-edit")
-      .find(".pages-section")
-      .should("be.visible");
+    cy.get("#document-edit").find(".pages-section").should("be.visible");
   });
 
   it("Shows as many thumbnails as the document has", () => {
     cy.getStore("document").then(($document) => {
-      const pages = $document.selectedDocument.pages
+      const pages = $document.selectedDocument.pages;
       const pagesLength = pages.length;
 
       cy.get("#document-edit")
-      .find(".pages-section")
-      .find(".edit-pages")
-      .find(".document-grid")
-      .find(".image-section")
-      .its("length")
-      .should("equal", pagesLength);
+        .find(".pages-section")
+        .find(".edit-pages")
+        .find(".document-grid")
+        .find(".image-section")
+        .its("length")
+        .should("equal", pagesLength);
     });
   });
 
   it("Shows the sidebar & buttons", () => {
-    cy.get("#document-edit")
-      .find(".sidebar")
-      .should("be.visible");
+    cy.get("#document-edit").find(".sidebar").should("be.visible");
 
     cy.get("#document-edit")
       .find(".sidebar")
@@ -49,8 +45,8 @@ describe("Document Edit", () => {
       .find(".smart-split")
       .should("be.visible");
 
-    cy.getStore("document").then($document => {
-      if($document.selectedDocument.proposed_split) {
+    cy.getStore("document").then(($document) => {
+      if ($document.selectedDocument.proposed_split) {
         cy.get("#document-edit")
           .find(".sidebar")
           .find(".smart-split")
@@ -86,14 +82,13 @@ describe("Document Edit", () => {
           .find(".smart-split")
           .find(".split-tooltip")
           .should("be.visible");
-        
       }
     });
   });
 
   it("Can toggle automatic splitting switch if there are splitting suggestions", () => {
-    cy.getStore("document").then($document => {
-      if($document.selectedDocument.proposed_split) {
+    cy.getStore("document").then(($document) => {
+      if ($document.selectedDocument.proposed_split) {
         cy.get("#document-edit")
           .find(".sidebar")
           .find(".smart-split")
@@ -113,7 +108,7 @@ describe("Document Edit", () => {
           .find(".lines")
           .should("have.class", "not-active-split");
 
-        cy.getStore("edit").then($edit => {
+        cy.getStore("edit").then(($edit) => {
           expect($edit.updatedDocument).to.have.lengthOf(1);
         });
       }
@@ -121,8 +116,8 @@ describe("Document Edit", () => {
   });
 
   it("Shows the splitting points if there are splitting suggestions", () => {
-    cy.getStore("document").then($document => {
-      if($document.selectedDocument.proposed_split) {
+    cy.getStore("document").then(($document) => {
+      if ($document.selectedDocument.proposed_split) {
         const split = $document.selectedDocument.proposed_split;
         const splitLength = split.length;
 
@@ -133,24 +128,23 @@ describe("Document Edit", () => {
           .find(".active-split")
           .its("length")
           .should("equal", splitLength);
-          
       }
     });
   });
 
   it("Shows the splitting lines by default when no suggestions, and the visible lines should be number of pages, and last one not visible", () => {
-    cy.getStore("document").then($document => {
-      const pages = $document.selectedDocument.pages
+    cy.getStore("document").then(($document) => {
+      const pages = $document.selectedDocument.pages;
       const pagesLength = pages.length;
 
-      if(!$document.selectedDocument.proposed_split) {
+      if (!$document.selectedDocument.proposed_split) {
         cy.get("#document-edit")
           .find(".pages-section")
           .find(".image-section")
           .find(".splitting-lines")
           .find(".lines")
           .should("have.class", "not-active-split")
-          .and($lines => {
+          .and(($lines) => {
             expect($lines).to.have.lengthOf(pagesLength);
             expect($lines[pagesLength - 1]).not.to.be.visible;
           });
@@ -159,14 +153,14 @@ describe("Document Edit", () => {
   });
 
   it("Clicking splitting lines creates new documents", () => {
-    cy.getStore("document").then($document => {
-      if($document.selectedDocument.proposed_split) {
+    cy.getStore("document").then(($document) => {
+      if ($document.selectedDocument.proposed_split) {
         cy.get("#document-edit")
           .find(".sidebar")
           .find(".smart-split")
           .find(".split-switch")
           .click();
-        
+
         cy.wait(1000);
       }
 
@@ -177,8 +171,8 @@ describe("Document Edit", () => {
         .find(".lines")
         .first()
         .click();
-      
-      cy.getStore("edit").then($edit => {
+
+      cy.getStore("edit").then(($edit) => {
         expect($edit.updatedDocument).to.have.lengthOf(2);
       });
     });
@@ -249,7 +243,7 @@ describe("Document Edit", () => {
       .find(".edit-mode-btn")
       .first()
       .should("not.be.disabled");
-    
+
     cy.get("#document-edit")
       .find(".pages-section")
       .find(".edit-pages")
@@ -289,10 +283,10 @@ describe("Document Edit", () => {
       .find(".edit-mode-btn")
       .first()
       .click();
-    
-    cy.getStore("edit").then($edit => {
+
+    cy.getStore("edit").then(($edit) => {
       expect($edit.pagesForPostprocess[0].angle).to.equal(-90);
-    })
+    });
   });
 
   it("Rotates all thumbnails", () => {
@@ -304,10 +298,29 @@ describe("Document Edit", () => {
       .last()
       .click();
 
-    cy.getStore("edit").then($edit => {
-      $edit.pagesForPostprocess.map(page => {
+    cy.getStore("edit").then(($edit) => {
+      $edit.pagesForPostprocess.map((page) => {
         expect(page.angle).to.equal(90);
-      })
+      });
+    });
+  });
+
+  it("Can drag and drop to reorder pages", () => {
+    cy.getStore("edit").then(($edit) => {
+      if ($edit.pagesForPostprocess.length > 1) {
+        const firstPageId = $edit.pagesForPostprocess[0].id;
+        cy.get(
+          "#document-edit .pages-section .edit-pages .document-grid .image-section:last-child"
+        ).drag(
+          "#document-edit .pages-section .edit-pages .document-grid .image-section:first-child"
+        );
+        cy.getStore("edit").then(($newEdit) => {
+          const newFirstPageId = $newEdit.pagesForPostprocess[0].id;
+          expect(newFirstPageId).to.not.equal(firstPageId);
+        });
+      } else {
+        cy.log("Can't test drag and drop, this document has only one page");
+      }
     });
   });
 });
