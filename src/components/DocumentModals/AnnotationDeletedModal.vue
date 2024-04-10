5450c7fc49a22d7a34c5a0b648d64fbaf6259d23
Pedro Dinis <pdinisricardo@gmail.com>
2024-04-10 23:10:43 +0100

    Implement annotation deleted popup for deep linking

---
+++ src/components/DocumentModals/AnnotationDeletedModal.vue
@@ -0,0 +1,38 @@
+<template>
+  <section class="viewport-modal">
+    <b-modal
+      v-model="isModalActive"
+      class="modal-text-center"
+      :width="500"
+      :can-cancel="false"
+    >
+      <section class="modal-card-body">
+        <div class="content">
+          <h3>{{ $t("annotation_deleted") }}</h3>
+        </div>
+      </section>
+      <footer class="modal-card-foot" style="padding-top: 0">
+        <b-button type="is-primary" class="primary-button" @click="closeModal">
+          {{ $t("ok") }}
+        </b-button>
+      </footer>
+    </b-modal>
+  </section>
+</template>
+
+<script>
+export default {
+  name: "AnnotationDeletedModal",
+  data() {
+    return {
+      isModalActive: window.location.hash === "#deleted",
+    };
+  },
+  methods: {
+    closeModal() {
+      window.location.hash = "";
+      this.isModalActive = false;
+    },
+  },
+};
+</script>
