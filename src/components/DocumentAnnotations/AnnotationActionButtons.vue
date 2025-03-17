580065a4530017a732d710ef291de19a89760b07
Pedro Dinis <pdinisricardo@gmail.com>
2025-03-17 21:41:36 +0000

    Fix issue with i18n locale

--- src/components/DocumentAnnotations/AnnotationActionButtons.vue
+++ src/components/DocumentAnnotations/AnnotationActionButtons.vue
@@ -206,9 +206,7 @@ export default {
   data() {
     return {
       tooltipDelay: 700,
-      showText:
-        window.innerWidth >
-        TEXT_BREAKPOINT_WIDTH(this.$i18n ? this.$i18n.locale : "en"),
+      showText: window.innerWidth > TEXT_BREAKPOINT_WIDTH,
     };
   },
   computed: {
@@ -233,8 +231,7 @@ export default {
   },
   methods: {
     resize() {
-      this.showText =
-        window.innerWidth > TEXT_BREAKPOINT_WIDTH(this.$i18n.locale);
+      this.showText = window.innerWidth > TEXT_BREAKPOINT_WIDTH;
     },
     search() {
       this.$emit("search");
