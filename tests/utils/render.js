f5b05160faca4c82c649b4fcd7a5143403666e8b
Pedro Dinis <pdinisricardo@gmail.com>
2025-03-17 22:32:13 +0000

    Fix test units for Vue 3 upgrade

--- tests/utils/render.js
+++ tests/utils/render.js
@@ -1,16 +1,41 @@
 import { mount, shallowMount } from "@vue/test-utils";
 import { $t } from "./i18n";
-import store from "../../src/store";
+import Buefy from "buefy";
+import VueObserveVisibility from "vue3-observe-visibility";
+import { init as initStore } from "./store";
+
+// Mock IntersectionObserver
+global.IntersectionObserver = class IntersectionObserver {
+  constructor() {}
+
+  disconnect() {
+    return null;
+  }
+
+  observe() {
+    return null;
+  }
+
+  takeRecords() {
+    return null;
+  }
+
+  unobserve() {
+    return null;
+  }
+};
 
 export const render = (component, shallow, props = {}, data = {}) => {
   const values = {
-    store,
     props,
-    mocks: {
-      $t,
-    },
     data() {
       return data;
+    },
+    global: {
+      mocks: {
+        $t,
+      },
+      plugins: [initStore(), Buefy, VueObserveVisibility],
     },
   };
 
