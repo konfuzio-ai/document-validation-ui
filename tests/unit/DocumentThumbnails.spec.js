ec459680aa5fa9a7b59bc0af0999253dd6e59aeb
Pedro Dinis <pdinisricardo@gmail.com>
2025-03-18 12:41:06 +0000

    Migrate unit tests to be compatible with vue test utils new version

--- tests/unit/DocumentThumbnails.spec.js
+++ tests/unit/DocumentThumbnails.spec.js
@@ -9,20 +9,20 @@ describe("Document Thumbnails Component", () => {
 
   it("check if first thumbnail is selected", async () => {
     const wrapper = render(DocumentThumbnails, true);
-    expect(await wrapper.findAll(".img-thumbnail").at(0).classes()).toContain(
+    expect(await wrapper.findAll(".img-thumbnail")[0].classes()).toContain(
       "selected"
     );
   });
 
   it("check if second thumbnail is not selected", async () => {
     const wrapper = render(DocumentThumbnails, true);
-    expect(
-      await wrapper.findAll(".img-thumbnail").at(1).classes()
-    ).not.toContain("selected");
+    expect(await wrapper.findAll(".img-thumbnail")[1].classes()).not.toContain(
+      "selected"
+    );
   });
 
   it("check if second thumbnail has text 2", async () => {
     const wrapper = render(DocumentThumbnails, true);
-    expect(await wrapper.findAll(".number-thumbnail").at(1).text()).toBe("2");
+    expect(await wrapper.findAll(".number-thumbnail")[1].text()).toBe("2");
   });
 });
