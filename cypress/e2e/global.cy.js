9ee55c57d26d17a7313aeeba9014b15c1f590817
Pedro Dinis <pdinisricardo@gmail.com>
2025-04-12 12:11:49 +0100

    Create e2e test for document rename

--- cypress/e2e/global.cy.js
+++ cypress/e2e/global.cy.js
@@ -11,12 +11,29 @@
 // please read our getting started guide:
 // https://on.cypress.io/introduction-to-cypress
 
-describe("Global DVUI tests", () => {
+describe("DVUI test script", () => {
   beforeEach(() => {
     // Cypress starts out with a blank slate for each test
     // so we must tell it to visit our website with the `cy.visit()` command.
     // Since we want to visit the same URL at the start of all our tests,
     // we include it in our beforeEach function so that it runs before each test
-    cy.visit("http://localhost:8080");
+    cy.visit("http://localhost:8080/d/1375095/");
+  });
+
+  it("Edit document name", () => {
+    const docName = `Test_${Date.now()}`;
+    cy.intercept({
+      method: "PATCH",
+      url: "api/v3/documents/*",
+      hostname: "testing.konfuzio.com",
+    }).as("renameDoc");
+
+    cy.get("#document-info .edit-btn.btn").click();
+    cy.get("#document-info .document-name")
+      .type("{selectall}{backspace}")
+      .type(docName);
+    cy.get("#document-info .save-btn.btn").click();
+    cy.wait("@renameDoc").its("response.statusCode").should("eq", 200);
+    cy.get("#document-info .document-name").contains(docName);
   });
 });
