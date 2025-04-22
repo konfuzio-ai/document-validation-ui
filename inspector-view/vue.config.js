9f85b5ae9bb475b048e786a84f5a3366470d5b57
fz <fz@konfuzio.com>
2025-04-22 15:38:59 +0200

    Update publicPath to root for local testing

--- inspector-view/vue.config.js
+++ inspector-view/vue.config.js
@@ -1,6 +1,6 @@
 module.exports = {
   lintOnSave: false,
-  publicPath: process.env.NODE_ENV === 'production' ? '/inspector-view/' : '/',
+  publicPath: '/',
   devServer: {
     port: 8081,
     proxy: {
