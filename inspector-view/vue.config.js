0b77bb0e0b771db164d1ddba37efdfb9f857aaba
fz <fz@konfuzio.com>
2025-04-22 15:36:51 +0200

    Fix environment variables injection in Vue app

--- inspector-view/vue.config.js
+++ inspector-view/vue.config.js
@@ -9,5 +9,17 @@ module.exports = {
         changeOrigin: true
       }
     }
+  },
+  chainWebpack: config => {
+    config.plugin('define').tap(args => {
+      const env = process.env;
+      args[0]['process.env'] = Object.keys(env).reduce((acc, key) => {
+        if (key.startsWith('VUE_APP_')) {
+          acc[key] = JSON.stringify(env[key]);
+        }
+        return acc;
+      }, {});
+      return args;
+    });
   }
 } 
